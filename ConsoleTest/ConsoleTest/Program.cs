34f9862ae6ea391179bf28fd48d0e95a81dd5797
johnC19457 <1945725762@qq.com>
2019-05-12 23:09:46 +0800

    test
    test

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("test");
             Console.WriteLine("Hello World!");
         }
     }
